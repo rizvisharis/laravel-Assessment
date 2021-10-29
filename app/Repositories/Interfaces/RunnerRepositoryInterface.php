f3dd6603fa6470fc16d310348b4e307b83185446
Rizvi <rizvisharis@gmail.com>
2021-10-29 13:18:47 +0530

    refactor the codes

--- app/Repositories/Interfaces/RunnerRepositoryInterface.php
+++ app/Repositories/Interfaces/RunnerRepositoryInterface.php
@@ -4,5 +4,13 @@ namespace App\Repositories\Interfaces;
 
 interface RunnerRepositoryInterface
 {
+    public function get($condition = null);
 
+    public function find($id);
+
+    public function create($requestData);
+
+    public function update($requestData);
+
+    public function delete($requestData);
 }
--- app/Services/Interfaces/RunnerServiceInterface.php
+++ app/Services/Interfaces/RunnerServiceInterface.php
@@ -4,5 +4,5 @@ namespace App\Services\Interfaces;
 
 interface RunnerServiceInterface
 {
-
+    public function index($requstData, $runnerId);
 }
